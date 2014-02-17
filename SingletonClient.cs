2032f2239ee303b1cd458c98f0df1d715bd02544
michaelzhuang <zhuangdeyouxiang@gmail.com>
2014-02-18 00:12:00 +1100

    complex version of Singleton

---
+++ SingletonClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Sandbox
+{
+    [TestFixture]
+    public class SingletonClient
+    {
+        [Test]
+        public void UseSingleton()
+        {
+            Singleton s1 = Singleton.Instance;
+            Singleton s2 = Singleton.Instance;
+            Assert.AreSame(s1,s2);
+        }
+    }
+}
---
+++ singleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    public class Singleton
+    {
+        private static readonly object mutex = new object();
+        private static volatile Singleton instance;
+        private Singleton()
+        {
+        
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (mutex)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                        
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public void DoSomeThing()
+        {
+            
+        }
+    }
+}
